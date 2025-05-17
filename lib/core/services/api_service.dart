633fde4f87559066c99b79e615d700ff4bae41e4
Yash Chaudhari <yashchaudhari109@users.noreply.github.com>
2025-05-17 13:49:42 +0530

    Your commit message here

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -200,8 +200,6 @@ class ApiService {
   // Entry point for isolate download
   static Future<void> _downloadEntryPoint(_DownloadParams params) async {
     final dio = Dio();
-    String yash;
-
     final response = await dio.download(
       params.url,
       params.savePath,
